5e87eacb0ff5110030d0dffc3fc204b186517d8d
ElliNet13 <elliotmoose4@gmail.com>
2024-03-22 01:52:46 +0000

    Initial commit

---
+++ .deno/gen/file/home/runner/Blog/index.ts.js
@@ -0,0 +1,15 @@
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+blog({
+    title: "ElliNet13's Deno Blog",
+    author: "ElliNet13",
+    // avatar: "./avatar.svg",
+    // avatarClass: "full",
+    links: [
+        {
+            title: "Links",
+            url: "https://bit.ly/m/ellinet13"
+        }, 
+    ],
+    background: "#fff"
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImZpbGU6Ly8vaG9tZS9ydW5uZXIvQmxvZy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgYmxvZyBmcm9tIFwiaHR0cHM6Ly9kZW5vLmxhbmQveC9ibG9nQDAuMy4zL2Jsb2cudHN4XCI7XG5cbmJsb2coe1xuICB0aXRsZTogXCJFbGxpTmV0MTMncyBEZW5vIEJsb2dcIixcbiAgYXV0aG9yOiBcIkVsbGlOZXQxM1wiLFxuICAvLyBhdmF0YXI6IFwiLi9hdmF0YXIuc3ZnXCIsXG4gIC8vIGF2YXRhckNsYXNzOiBcImZ1bGxcIixcbiAgbGlua3M6IFtcbiAgICB7IHRpdGxlOiBcIkxpbmtzXCIsIHVybDogXCJodHRwczovL2JpdC5seS9tL2VsbGluZXQxM1wiIH0sXG4gIF0sXG4gIGJhY2tncm91bmQ6IFwiI2ZmZlwiXG59KTsiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxJQUFJLE1BQU0seUNBQXlDLENBQUM7QUFFM0QsSUFBSSxDQUFDO0lBQ0gsS0FBSyxFQUFFLHVCQUF1QjtJQUM5QixNQUFNLEVBQUUsV0FBVztJQUNuQiwwQkFBMEI7SUFDMUIsdUJBQXVCO0lBQ3ZCLEtBQUssRUFBRTtRQUNMO1lBQUUsS0FBSyxFQUFFLE9BQU87WUFBRSxHQUFHLEVBQUUsNEJBQTRCO1NBQUU7S0FDdEQ7SUFDRCxVQUFVLEVBQUUsTUFBTTtDQUNuQixDQUFDLENBQUMifQ==
---
+++ index.ts
@@ -0,0 +1,12 @@
+import blog from "https://deno.land/x/blog@0.3.3/blog.tsx";
+
+blog({
+  title: "ElliNet13's Deno Blog",
+  author: "ElliNet13",
+  // avatar: "./avatar.svg",
+  // avatarClass: "full",
+  links: [
+    { title: "Links", url: "https://bit.ly/m/ellinet13" },
+  ],
+  background: "#fff"
+});
